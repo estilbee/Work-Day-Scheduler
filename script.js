9ee40ecacfbf6e307e1a57a5882468cf9193d4f6
estilbee <odaouphars@gmail.com>
2022-10-09 11:30:58 -0400

    added local storage to each time block

--- script.js
+++ script.js
@@ -13,6 +13,18 @@ $(saveBtn).on("click", function(){
 
 $("#9 .description").val(localStorage.getItem("9"))
 // repeat for every number^^
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
+$("#18 .description").val(localStorage.getItem("18"))
+$("#19 .description").val(localStorage.getItem("19"))
+$("#20 .description").val(localStorage.getItem("20"))
+$("#21 .description").val(localStorage.getItem("21"))
 
 $(".time-block").each(function(){
     var divTime = $(this).attr("id")
